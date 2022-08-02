b4e5b26727831c82f7f999ff472e7640c6c2265c
cosmicKj <kyungjun1213@gmail.com>
2022-08-02 21:48:06 +0900

    Feat: App 포트 번호 설정 변경

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import express, { Application, Request, Response } from "express";
 import notionRouter from "./routes/notion";
 
 const app: Application = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the shack!");
